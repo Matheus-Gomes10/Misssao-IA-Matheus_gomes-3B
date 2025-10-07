c06b9cf536f6049979acd538f9b9ec6c3c048959
Matheus-Gomes10 <matheus.gomes21@escola.pr.gov.br>
2025-10-07 12:00:15 -0300

    ggdg

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-
-
-
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const CaixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
